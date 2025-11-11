d0d291ae6d7ed25cd51a77986be103ed589a3153
hisakey <hogehoge@example.com>
2025-11-12 06:00:19 +0900

    feat: add seed data of user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create! do |u|
+  u.email = "test@example.com"
+  u.password = "password"
+  u.password_confirmation = "password"
+end
