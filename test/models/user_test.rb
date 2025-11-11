376cd9c0fddacd62434d92691c1e67872c8fb548
hisakey <hogehoge@example.com>
2025-11-12 06:00:05 +0900

    feat: add user model test

---
+++ test/models/user_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(
+      email: "test@example.com",
+      password: "securepassword", password_confirmation: "securepassword"
+    )
+  end
+
+  test "正常系: 有効なユーザー" do
+  assert @user.valid?
+  end
+
+  test "異常系: メールアドレスが空欄" do
+    user = User.new(email: "", password: "password", password_confirmation: "password")
+    assert_not user.valid?
+    assert_includes user.errors[:email], "can't be blank"
+  end
+
+  test "異常系: メールアドレスが重複" do
+    @user.save!
+    duplicate_email_user = User.new(
+      email: @user.email,
+      password: "anotherpassword",
+      password_confirmation: "anotherpassword"
+    )
+    assert_not duplicate_email_user.valid?
+    assert_includes duplicate_email_user.errors[:email], "has already been taken"
+  end
+
+  test "異常系: パスワードが空欄（新規作成時）" do
+    user = User.new(email: "test@example.com", password: "", password_confirmation: "")
+    assert_not user.valid?
+    assert_includes user.errors[:password], "can't be blank"
+  end
+end
