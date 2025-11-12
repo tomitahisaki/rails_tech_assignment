d6d0115568dee0d0dec3fa6aaffe202c53b32057
hisakey <hogehoge@example.com>
2025-11-13 06:25:45 +0900

    feat: testの追加

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class PhotoTest < ActiveSupport::TestCase
+  def setup
+    @user = User.create!(email: "test@example.com", password: "securepassword", password_confirmation: "securepassword")
+  end
+
   test "正常系: 有効な写真" do
-    photo = Photo.new(title: "Sample Photo")
+    photo = Photo.new(title: "Sample Photo", user: @user)
     photo.image.attach(
       io: File.open(file_fixture("sample_image.png")),
       filename: "sample_image.png",
@@ -13,7 +17,7 @@ class PhotoTest < ActiveSupport::TestCase
   end
 
   test "異常系: タイトルが空欄の場合" do
-    photo = Photo.new(title: "")
+    photo = Photo.new(title: "", user: @user)
     photo.image.attach(
       io: File.open(file_fixture("sample_image.png")),
       filename: "sample_image.png",
@@ -24,7 +28,7 @@ class PhotoTest < ActiveSupport::TestCase
   end
 
   test "異常系: タイトルが30文字の場合" do
-    photo = Photo.new(title: "a" * 30)
+    photo = Photo.new(title: "a" * 30, user: @user)
     photo.image.attach(
       io: File.open(file_fixture("sample_image.png")),
       filename: "sample_image.png",
@@ -34,7 +38,7 @@ class PhotoTest < ActiveSupport::TestCase
   end
 
   test "異常系: タイトルが31文字の場合" do
-    photo = Photo.new(title: "a" * 31)
+    photo = Photo.new(title: "a" * 31, user: @user)
     photo.image.attach(
       io: File.open(file_fixture("sample_image.png")),
       filename: "sample_image.png",
@@ -45,7 +49,7 @@ class PhotoTest < ActiveSupport::TestCase
   end
 
   test "異常系: 画像が添付されていない場合" do
-    photo = Photo.new(title: "Sample Photo")
+    photo = Photo.new(title: "Sample Photo", user: @user)
     assert_not photo.valid?
     assert_includes photo.errors[:image], "を添付してください"
   end
