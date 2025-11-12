6c0f525458f63ec93545e5b180128e3a71a48a28
hisakey <hogehoge@example.com>
2025-11-13 05:29:23 +0900

    feat: photoモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_11_123340) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_12_202813) do
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
