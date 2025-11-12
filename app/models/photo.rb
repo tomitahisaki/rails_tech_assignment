64ce0c774763dab96b40dea39086ab0a307241e7
hisakey <hogehoge@example.com>
2025-11-13 06:08:54 +0900

    feat: photoモデルにバリデーション追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,12 @@
 class Photo < ApplicationRecord
-validates :title, presence: true, length: { maximum: 30 }
+  has_one_attached :image
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validate :image_must_be_attached
+
+  private
+
+  def image_must_be_attached
+    errors.add(:image, "を添付してください") unless image.attached?
+  end
 end
--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -3,23 +3,50 @@ require "test_helper"
 class PhotoTest < ActiveSupport::TestCase
   test "正常系: 有効な写真" do
     photo = Photo.new(title: "Sample Photo")
+    photo.image.attach(
+      io: File.open(file_fixture("sample_image.png")),
+      filename: "sample_image.png",
+      content_type: "image/png"
+      )
+
     assert photo.valid?
   end
 
   test "異常系: タイトルが空欄の場合" do
     photo = Photo.new(title: "")
+    photo.image.attach(
+      io: File.open(file_fixture("sample_image.png")),
+      filename: "sample_image.png",
+      content_type: "image/png"
+      )
     assert_not photo.valid?
     assert_includes photo.errors[:title], "を入力してください"
   end
 
   test "異常系: タイトルが30文字の場合" do
     photo = Photo.new(title: "a" * 30)
+    photo.image.attach(
+      io: File.open(file_fixture("sample_image.png")),
+      filename: "sample_image.png",
+      content_type: "image/png"
+      )
     assert photo.valid?
   end
 
   test "異常系: タイトルが31文字の場合" do
     photo = Photo.new(title: "a" * 31)
+    photo.image.attach(
+      io: File.open(file_fixture("sample_image.png")),
+      filename: "sample_image.png",
+      content_type: "image/png"
+      )
     assert_not photo.valid?
     assert_includes photo.errors[:title], "は30文字以内で入力してください"
+  end
+
+  test "異常系: 画像が添付されていない場合" do
+    photo = Photo.new(title: "Sample Photo")
+    assert_not photo.valid?
+    assert_includes photo.errors[:image], "を添付してください"
   end
 end
