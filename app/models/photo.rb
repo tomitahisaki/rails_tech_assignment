d8e8d9eedf0e393675fc79aec7ca17907dcc4d42
hisakey <hogehoge@example.com>
2025-11-13 05:37:40 +0900

    feat: バリデーションの追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,3 @@
 class Photo < ApplicationRecord
+validates :title, presence: true, length: { maximum: 30 }
 end
--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "正常系: 有効な写真" do
+    photo = Photo.new(title: "Sample Photo")
+    assert photo.valid?
+  end
+
+  test "異常系: タイトルが空欄の場合" do
+    photo = Photo.new(title: "")
+    assert_not photo.valid?
+    assert_includes photo.errors[:title], "を入力してください"
+  end
+
+  test "異常系: タイトルが30文字の場合" do
+    photo = Photo.new(title: "a" * 30)
+    assert photo.valid?
+  end
+
+  test "異常系: タイトルが31文字の場合" do
+    photo = Photo.new(title: "a" * 31)
+    assert_not photo.valid?
+    assert_includes photo.errors[:title], "は30文字以内で入力してください"
+  end
 end
