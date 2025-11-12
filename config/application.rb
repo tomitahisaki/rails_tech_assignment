e8e6bd57ba611e0a3bb00d16ad256a317c97118d
hisakey <hogehoge@example.com>
2025-11-12 20:32:50 +0900

    feat: i18nの日本語対応

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module RailsTechAssignment
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
